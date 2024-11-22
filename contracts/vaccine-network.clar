5455b095df7de17d35d5d1f2d7d1786a780e9efa
Midorichie <hamsohood@gmail.com>
2024-11-22 10:22:11 +0100

    final_commit

---
+++ contracts/vaccine-network.clar
@@ -0,0 +1,5 @@
+;; vaccine-network
+(define-data-var example-var uint u0)
+
+(define-public (example-function (input uint))
+    (ok input))
---
+++ tests/deps.ts
@@ -0,0 +1,7 @@
+export {
+    Clarinet,
+    Tx,
+    Chain,
+    Account,
+    types
+} from 'https://deno.land/x/clarinet@v1.5.4/index.ts';
